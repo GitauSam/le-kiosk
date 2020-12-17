53c46551cacbab63f0e578d422772bf40e326a43
GitauSam <njorogegitausamuel@gmail.com>
2020-12-17 16:12:11 +0300

    Starts implementing checkout process

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -43,7 +43,9 @@ const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart
                             size="large" 
                             type="button"
                             variant="contained"
-                            color="primary">
+                            color="primary"
+                            component={Link}
+                            to="/checkout">
                             Checkout
                     </Button>
                 </div>
