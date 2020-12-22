e9aaddef02809cce99a4e7d2f0075be9409e413d
GitauSam <njorogegitausamuel@gmail.com>
2020-12-22 16:18:15 +0300

    Starts implementing stripe configuration

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,4 +1,4 @@
 import Commerce from '@chec/commerce.js';
 
 
-export const commerce = new Commerce('pk_20133bfe6dca94a73226cc8c8142a880ace8ca44fe897', true);
+export const commerce = new Commerce('pk_20133d8638112c8e73fe0e6ba8b223063cd03f5489445', true);
